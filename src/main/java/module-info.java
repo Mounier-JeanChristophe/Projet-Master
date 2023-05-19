0e271cf70f116c76e59dd438c6d0c9ab0be99577
anthony-vitrant <anthony.vitrant@almuni.univ-avignon.fr>
2023-05-19 17:55:05 +0200

    ERREUR fusion en cours javax nik

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,14 @@
-module com.example.optimisationcompetitionssportives {
+module Optimisation.Competitions.Sportives {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.json;
     requires javafx.graphics;
     requires cplex;
     requires java.sql;
+    requires decimal4j;
+    requires com.google.common;
+    requires gs.core;
+    requires gs.algo;
 
 
     opens com.example.optimisationcompetitionssportives to javafx.fxml;
